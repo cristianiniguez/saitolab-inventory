fc4ab2268e9a7447c391744ce8e34f3ec3083052
cristianiniguez <felizpc1@gmail.com>
2020-09-10 15:33:12 -0400

    Creating products and transactions windows

--- src/index.js
+++ src/index.js
@@ -1,20 +1,53 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 let mainWindow
 let productsWindow
 let transactionsWindow
 
-const createMainWindow = () => {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-  mainWindow.loadFile(path.join(__dirname, 'views', 'main-window', 'index.html'));
-  mainWindow.webContents.openDevTools();
-};
+const startWindowOptions = {
+  width: 800,
+  height: 600,
+  webPreferences: {
+    nodeIntegration: true
+  },
+  show: false
+}
 
-app.on('ready', createMainWindow);
+const createWindows = () => {
+  mainWindow = new BrowserWindow(startWindowOptions)
+  productsWindow = new BrowserWindow(startWindowOptions)
+  transactionsWindow = new BrowserWindow(startWindowOptions)
+  
+  mainWindow.loadFile(path.join(__dirname, 'views', 'main-window', 'index.html'));
+  productsWindow.loadFile(path.join(__dirname, 'views', 'products-window', 'index.html'));
+  transactionsWindow.loadFile(path.join(__dirname, 'views', 'transactions-window', 'index.html'));
+  
+  mainWindow.on('close', () => {
+    app.quit()
+  })
+}
+
+const showWindow = (win) => {
+  win.show()
+}
+
+app.on('ready', () => {
+  createWindows()
+  showWindow(mainWindow)
+  mainWindow.webContents.openDevTools();
+});
+
+ipcMain.on('open-window', (event, args) => {
+  console.log(args)
+  switch (args) {
+    case 'products':
+      showWindow(productsWindow)
+      break
+    case 'transactions':
+      showWindow(transactionsWindow)
+      break
+    default:
+      console.log('I dont know what are you saying')
+  }
+})
--- src/views/main-window/main.js
+++ src/views/main-window/main.js
@@ -1,3 +1,4 @@
+const { ipcRenderer } = require('electron')
 const moment = require('moment')
 
 const Product = require('../../models/product.model.js')
@@ -6,6 +7,9 @@ const Transaction = require('../../models/transaction.model.js')
 // Elements
 const inp_startDate = document.getElementById('start-date')
 const inp_endDate = document.getElementById('end-date')
+
+const $btn_productsWindow = document.getElementById('products-window')
+const $btn_transactionsWindow = document.getElementById('transactions-window')
 
 // Function
 function setToday() {
@@ -17,4 +21,12 @@ function setToday() {
 window.addEventListener('load', () => {
   setToday()
   console.log(inp_startDate.value, inp_endDate.value)
+})
+
+// Events
+$btn_productsWindow.addEventListener('click', () => {
+  ipcRenderer.send('open-window', 'products')
+})
+$btn_transactionsWindow.addEventListener('click', () => {
+  ipcRenderer.send('open-window', 'transactions')
 })
